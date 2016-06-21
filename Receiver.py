63f67a9e624b23a878aea17f5dbbcaa2d46636a3
daniwhite <white.dani.m@gmail.com>
2016-06-21 15:39:09 -0700

    Add int cast to rssi value passed by find_rssi, which fixes problems with comparisons

--- Receiver.py
+++ Receiver.py
@@ -1,12 +1,26 @@
 #Program for receiver RPi
 
 import subprocess
+import time
 
 beacon_addr = 'B8:27:EB:38:A7:AE' #MAC address of beacon RPi
+
+approaching_rssi = -5 #rssi above this value says biker is approaching
+arrived_rssi = -1 #rssi above this value says biker has approaching
 
 def find_rssi():
     rssi = subprocess.check_output("hcitool rssi %s" % beacon_addr,shell=True)
     rssi = rssi[19:len(rssi) -1]
-    return rssi
+    return int(rssi)
 
-print find_rssi()
+while(True): #main loop of program
+    rssi = find_rssi()
+    if rssi > arrived_rssi:
+        print "Arrived!"
+    else:
+        if rssi > approaching_rssi:
+            print "Approaching"
+        else:
+            print "Out of range"
+    print rssi
+    time.sleep(1) #Not necessary, just makes reading input easier
