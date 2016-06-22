b346f76f6b88bc800e5547c1c77ed09a26dc5dd6
daniwhite <white.dani.m@gmail.com>
2016-06-22 13:57:27 -0700

    Comment out prints, switch them to GrovePi LCD prints

--- Receiver.py
+++ Receiver.py
@@ -2,8 +2,13 @@
 
 import subprocess
 import time
+import grovepi
+import grove_rgb_lcd as lcd
 
 beacon_addr = 'B8:27:EB:38:A7:AE' #MAC address of beacon RPi
+light_port = 3 #LED should be put in port D3
+
+grovepi.pinMode(light_port, "OUTPUT")
 
 approaching_rssi = -5 #rssi above this value says biker is approaching
 arrived_rssi = -1 #rssi above this value says biker has approaching
@@ -16,11 +21,16 @@ def find_rssi():
 while(True): #main loop of program
     rssi = find_rssi()
     if rssi > arrived_rssi:
-        print "Arrived!"
+        lcd.setText("Arrived!\n RSSI:%d" % rssi)
+        grovepi.digitalWrite(light_port,1)
     else:
         if rssi > approaching_rssi:
-            print "Approaching"
+            lcd.setText("Approaching...\n RSSI:%d" % rssi)
+            grovepi.digitalWrite(light_port,1)
+            time.sleep(1)
+            grovepi.digitalWrite(light_port,0)
         else:
-            print "Out of range"
-    print rssi
-    time.sleep(1) #Not necessary, just makes reading input easier
+            lcd.setText("Out of range\n RSSI:%d" % rssi)
+            grovepi.digitalWrite(light_port,0)
+#    print rssi
+    #time.sleep(1) #Not necessary, just makes reading input easier
