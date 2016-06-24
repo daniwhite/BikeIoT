c784949761a04f82a8c795859c94aed716211ac8
daniwhite <white.dani.m@gmail.com>
2016-06-24 08:57:47 -0700

    Move code for setting outputs based on rssi to their own functions, add function to compute average rssi

--- Receiver.py
+++ Receiver.py
@@ -27,7 +27,9 @@ grovepi.ledBar_init(ledbar_port, 0)
 approaching_rssi = -5 # rssi above this value says biker is approaching
 arrived_rssi = -1 # rssi above this value says biker has approaching
 
-def find_rssi():
+rssi_buffer = [-40,-40,-40]
+
+def find_raw_rssi():
     # Create process to find rssi
     rssiProc = subprocess.Popen(['hcitool', 'rssi', beacon_addr],
         stdout=subprocess.PIPE)
@@ -35,6 +37,15 @@ def find_rssi():
     rssi = rssiProc.stdout.readline()
     rssi = rssi[19:len(rssi) -1]
     return int(rssi)
+
+def find_avg_rssi():
+    rssi_buffer[2] = rssi_buffer[1]
+    rssi_buffer[1] = rssi_buffer[0]
+    rssi_buffer[0] = find_raw_rssi()
+    sum = 0
+    for i in rssi_buffer:
+        sum += i
+    return sum / 3
 
 # Still being fine tuned
 def set_LEDBar(rssi):
@@ -49,28 +60,35 @@ def set_LEDBar(rssi):
     grovepi.ledBar_setLevel(ledbar_port, rssi)
     return rssi
 
+def set_LED(rssi):
+    if rssi > arrived_rssi:
+        grovepi.digitalWrite(light_port,1)
+    elif rssi > approaching_rssi:
+        grovepi.digitalWrite(light_port,1)
+        time.sleep(0.5)
+        grovepi.digitalWrite(light_port,0)
+        time.sleep(0.5)
+    else:
+        grovepi.digitalWrite(light_port,0)
+
+def set_LCD(raw_rssi, adj_rssi):
+    if rssi > arrived_rssi:
+        lcd.setText("Arrived!\n RAW:%d ADJ:%d" % (rssi, adj_rssi))
+        lcd.setRGB(0,255,0)
+    elif rssi > approaching_rssi:
+        lcd.setText("Approaching...\n RAW:%d ADJ:%d" % (rssi, adj_rssi))
+        lcd.setRGB(0,127,0)
+    else:
+        lcd.setText("Out of rang\n RAW:%d ADJ:%d" % (rssi, adj_rssi))
+        lcd.setRGB(0,127,0)
+
 # Main loop of program
 while(True):
     try:
-        rssi = find_rssi()
+        rssi = find_avg_rssi()
         adj_rssi = set_LEDBar(rssi)
-        if rssi > arrived_rssi:
-            lcd.setText("Arrived!\n RSSI:%d    %d" % (rssi, adj_rssi))
-            lcd.setRGB(0,255,0)
-            grovepi.digitalWrite(light_port,1)
-        else:
-            if rssi > approaching_rssi:
-                lcd.setText("Approaching...\n RSSI:%d    %d" % (rssi, adj_rssi))
-                grovepi.digitalWrite(light_port,1)
-                lcd.setRGB(0,255,0)
-                time.sleep(1)
-                grovepi.digitalWrite(light_port,0)
-                lcd.setRGB(255,255,255)
-                time.sleep(1)
-            else:
-                lcd.setText("Out of range\n RSSI:%d    %d" % (rssi, adj_rssi))
-                grovepi.digitalWrite(light_port,0)
-                lcd.setRGB(255,255,255)
+        set_LED(adj_rssi)
+        set_LCD(rssi,adj_rssi)
 
     #If beacon is not connected, find_rssi() throws ValueError when it tries to
     #cast an empty string to int
