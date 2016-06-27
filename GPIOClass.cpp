8847b93eb892d6200b56fa5bea8cf15a80f2b32e
daniwhite <white.dani.m@gmail.com>
2016-06-27 14:55:34 -0700

    Switched out GPIOClass.cpp for this older version: https://github.com/halherta/RaspberryPi-GPIOClass-v1

--- GPIOClass.cpp
+++ GPIOClass.cpp
@@ -1,181 +1,99 @@
-
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
 #include "GPIOClass.h"
 
 using namespace std;
 
-GPIOClass::GPIOClass():valuefd(-1),directionfd(-1),exportfd(-1),unexportfd(-1),gpionum("4")
+GPIOClass::GPIOClass()
 {
-        //GPIO4 is default
-	this->export_gpio();
+	this->gpionum = "4"; //GPIO4 is default
 }
 
-GPIOClass::GPIOClass(string gnum):valuefd(-1),directionfd(-1),exportfd(-1),unexportfd(-1),gpionum(gnum)
+GPIOClass::GPIOClass(string gnum)
 {
-	//Instatiate GPIOClass object for GPIO pin number "gnum"
-	this->export_gpio();
+	this->gpionum = gnum;  //Instatiate GPIOClass object for GPIO pin number "gnum"
 }
-
-GPIOClass::~GPIOClass()
-{
-	this->unexport_gpio();
-}
-
 
 int GPIOClass::export_gpio()
 {
-	int statusVal = -1;
-	string exportStr = "/sys/class/gpio/export";
-	this->exportfd = statusVal = open(exportStr.c_str(),  O_WRONLY|O_SYNC);
-	if (statusVal < 0){
-		perror("could not open SYSFS GPIO export device");
-        exit(1);
+	string export_str = "/sys/class/gpio/export";
+	ofstream exportgpio(export_str.c_str()); // Open "export" file. Convert C++ string to C string. Required for all Linux pathnames
+	if (exportgpio < 0){
+		cout << " OPERATION FAILED: Unable to export GPIO"<< this->gpionum <<" ."<< endl;
+		return -1;
 	}
 
-	stringstream ss;
-	ss << this->gpionum;
-	string numStr = ss.str();
-	statusVal = write(this->exportfd, numStr.c_str(), numStr.length());
-	if (statusVal < 0){
-		perror("could not write to SYSFS GPIO export device");
-        exit(1);
-	}
-
-	statusVal = close(this->exportfd);
-	if (statusVal < 0){
-		perror("could not close SYSFS GPIO export device");
-        exit(1);
-	}
-
-    return statusVal;
+	exportgpio << this->gpionum ; //write GPIO number to export
+    exportgpio.close(); //close export file
+    return 0;
 }
 
 int GPIOClass::unexport_gpio()
 {
-	int statusVal = -1;
-	string unexportStr = "/sys/class/gpio/unexport";
-	this->unexportfd = statusVal = open(unexportStr.c_str(),  O_WRONLY|O_SYNC);
-	if (statusVal < 0){
-		perror("could not open SYSFS GPIO unexport device");
-        exit(1);
+	string unexport_str = "/sys/class/gpio/unexport";
+	ofstream unexportgpio(unexport_str.c_str()); //Open unexport file
+	if (unexportgpio < 0){
+		cout << " OPERATION FAILED: Unable to unexport GPIO"<< this->gpionum <<" ."<< endl;
+		return -1;
 	}
 
-	stringstream ss;
-	ss << this->gpionum;
-	string numStr = ss.str();
-	statusVal = write(this->unexportfd, numStr.c_str(), numStr.length());
-	if (statusVal < 0){
-		perror("could not write to SYSFS GPIO unexport device");
-        exit(1);
-	}
-
-	statusVal = close(this->unexportfd);
-	if (statusVal < 0){
-		perror("could not close SYSFS GPIO unexport device");
-        exit(1);
-	}
-
-	return statusVal;
+	unexportgpio << this->gpionum ; //write GPIO number to unexport
+    unexportgpio.close(); //close unexport file
+    return 0;
 }
 
 int GPIOClass::setdir_gpio(string dir)
 {
-	int statusVal = -1;
-	string setdirStr ="/sys/class/gpio/gpio" + this->gpionum + "/direction";
 
-
-	this->directionfd = statusVal = open(setdirStr.c_str(), O_WRONLY|O_SYNC); // open direction file for gpio
-		if (statusVal < 0){
-			perror("could not open SYSFS GPIO direction device");
-        exit(1);
+	string setdir_str ="/sys/class/gpio/gpio" + this->gpionum + "/direction";
+	ofstream setdirgpio(setdir_str.c_str()); // open direction file for gpio
+		if (setdirgpio < 0){
+			cout << " OPERATION FAILED: Unable to set direction of GPIO"<< this->gpionum <<" ."<< endl;
+			return -1;
 		}
 
-	if (dir.compare("in") != 0 && dir.compare("out") != 0 ) {
-		fprintf(stderr, "Invalid direction value. Should be \"in\" or \"out\". \n");
-		exit(1);
-	}
-
-	statusVal = write(this->directionfd, dir.c_str(), dir.length());
-	if (statusVal < 0){
-		perror("could not write to SYSFS GPIO direction device");
-        exit(1);
-	}
-
-	statusVal = close(this->directionfd);
-	if (statusVal < 0){
-		perror("could not close SYSFS GPIO direction device");
-        exit(1);
-	}
-
-	    return statusVal;
+		setdirgpio << dir; //write direction to direction file
+		setdirgpio.close(); // close direction file
+	    return 0;
 }
 
 
 int GPIOClass::setval_gpio(string val)
 {
 
-    int statusVal = -1;
-	string setValStr = "/sys/class/gpio/gpio" + this->gpionum + "/value";
+	string setval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
+	ofstream setvalgpio(setval_str.c_str()); // open value file for gpio
+		if (setvalgpio < 0){
+			cout << " OPERATION FAILED: Unable to set the value of GPIO"<< this->gpionum <<" ."<< endl;
+			return -1;
+		}
 
-	this->valuefd = statusVal = open(setValStr.c_str(), O_WRONLY|O_SYNC);
-	if (statusVal < 0){
-			perror("could not open SYSFS GPIO value device");
-        exit(1);
-	}
-
-	if (val.compare("1") != 0 && val.compare("0") != 0 ) {
-		fprintf(stderr, "Invalid  value. Should be \"1\" or \"0\". \n");
-		exit(1);
-	}
-
-	statusVal = write(this->valuefd, val.c_str(), val.length());
-	if (statusVal < 0){
-		perror("could not write to SYSFS GPIO value device");
-        exit(1);
-	}
-
-	statusVal = close(this->valuefd);
-	if (statusVal < 0){
-		perror("could not close SYSFS GPIO value device");
-        exit(1);
-	}
-
-	    return statusVal;
+		setvalgpio << val ;//write value to value file
+		setvalgpio.close();// close value file
+	    return 0;
 }
 
 
 int GPIOClass::getval_gpio(string& val){
 
-	string getValStr = "/sys/class/gpio/gpio" + this->gpionum + "/value";
-	char buff[10];
-	int statusVal = -1;
-	this->valuefd = statusVal = open(getValStr.c_str(), O_RDONLY|O_SYNC);
-	if (statusVal < 0){
-			perror("could not open SYSFS GPIO value device");
-        exit(1);
-	}
+	string getval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
+	ifstream getvalgpio(getval_str.c_str());// open value file for gpio
+	if (getvalgpio < 0){
+		cout << " OPERATION FAILED: Unable to get value of GPIO"<< this->gpionum <<" ."<< endl;
+		return -1;
+			}
 
-	statusVal = read(this->valuefd, &buff, 1);
-	if (statusVal < 0){
-		perror("could not read SYSFS GPIO value device");
-        exit(1);
-	}
+	getvalgpio >> val ;  //read gpio value
 
-	buff[1]='\0';
+	if(val != "0")
+		val = "1";
+	else
+		val = "0";
 
-	val = string(buff);
-
-	if (val.compare("1") != 0 && val.compare("0") != 0 ) {
-		fprintf(stderr, "Invalid  value read. Should be \"1\" or \"0\". \n");
-		exit(1);
-	}
-
-	statusVal = close(this->valuefd);
-	if (statusVal < 0){
-		perror("could not close SYSFS GPIO value device");
-        exit(1);
-	}
-
-	return statusVal;
+	getvalgpio.close(); //close the value file
+    return 0;
 }
 
 
