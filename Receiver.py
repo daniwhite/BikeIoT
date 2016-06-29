e0ff2a32899d853ea1782e4567ea9b02d4dbb2a9
daniwhite <white.dani.m@gmail.com>
2016-06-29 16:50:10 -0700

    Much less blinking

--- Receiver.py
+++ Receiver.py
@@ -17,18 +17,37 @@ LOOP_ON = '01'
 LOOP_OFF = '00'
 
 # Initalize GPIO
-approaching_light = 15
-arrived_light = 14
+connection_light = 15
+loop_light = 14
+
+last_connectionState = False
+last_loop_state = False
 
 GPIO.setmode(GPIO.BCM)  # GPIO will use Broadcom pin numbers
 GPIO.setwarnings(False)
 
-GPIO.setup(approaching_light, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(arrived_light, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(connection_light, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(loop_light, GPIO.OUT, initial=GPIO.LOW)
 
 # Initalize bluetooth
 sc = btle.Scanner(0)
 beacon_addr = 'b8:27:eb:38:a7:ae'  # MAC address of beacon
+
+
+# Function that accounts for past light states, returns true if light is on
+def setLight(currentState, lastState, light):
+    if currentState:
+        print "Light %d currently true" % light
+        GPIO.output(light, GPIO.HIGH)
+        return True
+    elif lastState:
+        print "Light %d caught by last state" % light
+        GPIO.output(light, GPIO.HIGH)
+        return True
+    else:
+        print "Light %d off" % light
+        GPIO.output(light, GPIO.LOW)
+        return False
 
 # Main loop of program
 while(True):
@@ -37,17 +56,34 @@ while(True):
     for i, d in enumerate(devices):
         if d.addr == beacon_addr:
             beacon_index = i
-    if (beacon_index > -1):
+    data = devices[beacon_index].getValueText(1)
+    if setLight(beacon_index > -1, last_connectionState, connection_light):
+        print data
+        setLight(data == LOOP_ON, last_loop_state, loop_light)
+    last_connectionState = beacon_index > -1
+    last_loop_state = data == LOOP_ON
+'''
+    connectionState = beacon_index > -1
+    if  or last_connectionState:
         print "Beacon found!"
-        GPIO.output(approaching_light, GPIO.HIGH)
-
         data = devices[beacon_index].getValueText(1)
+        if beacon_index == -1:
+
+            if last_loop_state:
+                print "Caught by last_loop_state"
+                data = LOOP_ON
+                print "On"
+        GPIO.output(connection_light, GPIO.HIGH)
+        print data
         if data == LOOP_ON:
-            GPIO.output(arrived_light, GPIO.HIGH)
+            GPIO.output(loop_light, GPIO.HIGH)
+            last_loop_state = True
             print "On"
         else:
-            GPIO.output(arrived_light, GPIO.LOW)
+            GPIO.output(loop_light, GPIO.LOW)
+            last_loop_state = False
             print "Off"
     else:
-        print "Out of range."
-        GPIO.output(approaching_light, GPIO.LOW)
+
+    last_connectionState = beacon_index > -1
+'''
