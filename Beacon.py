8536211f419c396db9ce4f99818d9cba151708f7
daniwhite <white.dani.m@gmail.com>
2016-07-29 14:10:43 -0700

    Move broadcast code into a while loop

--- Beacon.py
+++ Beacon.py
@@ -6,11 +6,12 @@ LOOP_ON = '01'
 LOOP_OFF = '00'
 cmdstring = 'sudo hcitool -i hci0 cmd 0x08 0x0008 06 02 01 '
 
-loop_state = input("Loop state: ")  # Temporary
+while(True):
+    loop_state = input("Loop state: ")  # Temporary
 
-if loop_state:
-        cmdstring = cmdstring + LOOP_ON
-else:
-    cmdstring = cmdstring + LOOP_OFF
+    if loop_state:
+            cmdstring = cmdstring + LOOP_ON
+    else:
+        cmdstring = cmdstring + LOOP_OFF
 
-broadcastProc = subprocess.call(cmdstring, shell=True)
+    broadcastProc = subprocess.call(cmdstring, shell=True)
