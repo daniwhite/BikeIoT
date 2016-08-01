a1881b0d945d17a936f1d23be0f7fb19fca59580
daniwhite <white.dani.m@gmail.com>
2016-08-01 15:31:38 -0700

    Add code for alive broadcast to beacon code

--- Beacon.py
+++ Beacon.py
@@ -1,17 +1,39 @@
 # Program for beacon RPi
 
 import subprocess
+import serial
+import time
 
 LOOP_ON = '01'
 LOOP_OFF = '00'
+
+# Send bluetooth message
 cmdstring = 'sudo hcitool -i hci0 cmd 0x08 0x0008 06 02 01 '
 
-while(True):
-    loop_state = input("Loop state: ")  # Temporary
+loop_state = input("Loop state: ")  # Temporary
 
-    if loop_state:
+# Initialize serial (to control mDot)
+device = '/dev/ttyUSB0'
+baudrate = 115200
+ser = serial.Serial(device, baudrate)
+ser.write('AT+JOIN\n')
+
+aliveCntr = 0  # Counter to broadcast alive signal only every 30 seconds
+
+
+def getLoopState():
+    return loop_state  # Will eventually do something snazzier
+
+while(True):
+    if getLoopState():
             cmdstring = cmdstring + LOOP_ON
     else:
         cmdstring = cmdstring + LOOP_OFF
-
     broadcastProc = subprocess.call(cmdstring, shell=True)
+
+    # Send "I'm alive" signal over LoRa
+    if aliveCntr % 15 == 0:  # Broadcast only every 30 (15*2) seconds
+        ser.write("AT+SEND=I'm alive\n")
+
+    time.sleep(2)
+    aliveCntr += 1
--- mDot-Control.py
+++
@@ -1,6 +0,0 @@
-import serial
-device = "/dev/ttyUSB0"
-baudrate = 115200
-ser = serial.Serial(device, baudrate)
-ser.write("AT+JOIN\n")
-ser.write("AT+SEND=Hello\n")
