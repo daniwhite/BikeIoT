474363fc3e37bfc21731cdaebdf63b42305c7e99
daniwhite <white.dani.m@gmail.com>
2016-08-05 14:43:17 -0700

    More consistent light system using buffers

--- Receiver.py
+++ Receiver.py
@@ -17,48 +17,59 @@ GPIO.setup(connection_light, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(loop_light, GPIO.OUT, initial=GPIO.LOW)
 
 # Initalize bluetooth
-SCAN_LEN = 1.5
-PERSIST_LEN = 10
+SCAN_LEN = 0.5
+CONNECTION_PERSIST_LEN = 6
+LOOP_PERSIST_LEN = 1
 key = '42696379636c65'  # Special header on bluetooth message sent by beacon
 sc = btle.Scanner(0)
-sc.start()
-PERSIST_BUFFER_LEN = 3
-cycles = 0
-lastBeaconState = False
+scanBuf = []
+dataBuf = []
 
 # Main loop of program
 try:
     while(True):
-        print 'Cycles: %d' % cycles
-        print lastBeaconState
-        sc.process(SCAN_LEN)
-        devices = sc.getDevices()
+        scan = sc.scan(SCAN_LEN)
+        scanBuf.append(scan)
+        if(len(scanBuf) > CONNECTION_PERSIST_LEN):
+            scanBuf.pop(0)
         data = ''
-        print "Device count: %d" % len(devices)
-        for d in (devices):
+        for d in scan:
             print d.getValueText(7)
             msg = d.getValueText(7)
             if (not (msg is None)):
                 if msg[:len(msg) - 2] == key:
                     data = msg[len(msg) - 2:]
+                    if not (data == ''):
+                        dataBuf.append(data)
                     break
+
+        beaconDetected = False
+        for s in scanBuf:
+            print 'For loop endered'
+            for d in s:
+                print 'Inner loop entered'
+                msg = d.getValueText(7)
+                print 'Value text retreived'
+                print msg
+                if (not (msg is None)):
+                    print 'Confirmed it is not none'
+                    if msg[:len(msg) - 2] == key:
+                        print 'Checked for key'
+                        beaconDetected = True
+
+        print beaconDetected
+        # Keep buffer at correct length
+        if len(dataBuf) > LOOP_PERSIST_LEN:
+            dataBuf.pop(0)
+        loop_state = LOOP_ON in dataBuf
+
         # Set lights
-        beaconDetected = not (data == '')
         print 'Connection light: %s' % beaconDetected
-        if(cycles > PERSIST_BUFFER_LEN):
-            GPIO.output(connection_light, beaconDetected)
-        elif lastBeaconState:
-            GPIO.output(connection_light, True)
-        print data
-        loop_state = data == LOOP_ON
+        GPIO.output(connection_light, beaconDetected)
+
         print 'Loop light: %s' % loop_state
         GPIO.output(loop_light, loop_state)
         print
-        if (cycles > PERSIST_LEN):
-            lastBeaconState = beaconDetected
-            sc.clear()
-            cycles = -1
-        cycles += 1
 except btle.BTLEException:
     print 'Must run as root user'
 except:
