e32164bdde2acb6eb100f017cfc449060c9b750c
daniwhite <white.dani.m@gmail.com>
2016-08-05 13:22:51 -0700

    Sort of fixed blinking problems by checking against previous light states

--- Receiver.py
+++ Receiver.py
@@ -17,16 +17,26 @@ GPIO.setup(connection_light, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(loop_light, GPIO.OUT, initial=GPIO.LOW)
 
 # Initalize bluetooth
-sc = btle.Scanner(0)
-SCAN_LEN = 2  # Shortest length that consistantly picks up signal
+SCAN_LEN = 1.5
+PERSIST_LEN = 10
 key = '42696379636c65'  # Special header on bluetooth message sent by beacon
+sc = btle.Scanner(0)
+sc.start()
+PERSIST_BUFFER_LEN = 3
+cycles = 0
+lastBeaconState = False
 
 # Main loop of program
 try:
     while(True):
-        devices = sc.scan(SCAN_LEN)
+        print 'Cycles: %d' % cycles
+        print lastBeaconState
+        sc.process(SCAN_LEN)
+        devices = sc.getDevices()
         data = ''
+        print "Device count: %d" % len(devices)
         for d in (devices):
+            print d.getValueText(7)
             msg = d.getValueText(7)
             if (not (msg is None)):
                 if msg[:len(msg) - 2] == key:
@@ -35,13 +45,20 @@ try:
         # Set lights
         beaconDetected = not (data == '')
         print 'Connection light: %s' % beaconDetected
-        GPIO.output(connection_light, beaconDetected)
-
+        if(cycles > PERSIST_BUFFER_LEN):
+            GPIO.output(connection_light, beaconDetected)
+        elif lastBeaconState:
+            GPIO.output(connection_light, True)
         print data
         loop_state = data == LOOP_ON
         print 'Loop light: %s' % loop_state
         GPIO.output(loop_light, loop_state)
         print
+        if (cycles > PERSIST_LEN):
+            lastBeaconState = beaconDetected
+            sc.clear()
+            cycles = -1
+        cycles += 1
 except btle.BTLEException:
     print 'Must run as root user'
 except:
